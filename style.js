a41f065c14a9f25929f97efafb2c9e664d5390fd
Developer420ahmet <134101244+Developer420ahmet@users.noreply.github.com>
2023-06-18 13:39:40 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,59 @@
+function changeImgR() {
+    var img = document.getElementById('changer');
+    img.src = 'pizza.png';
+    var documentRight = document.getElementById('info-text-1');
+    documentRight.style.display = 'flex';
+    var mainImg = document.getElementById('info-text-0');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-2');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-3');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-4');
+    mainImg.style.display = 'none';
+}
+function changeImgL() {
+    var img_left = document.getElementById('changer');
+    img_left.src = 'checken.png';
+    var documentLeft = document.getElementById('info-text-2');
+    documentLeft.style.display = 'flex';
+    var mainImg = document.getElementById('info-text-0');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-1');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-3');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-4');
+    mainImg.style.display = 'none';
+}
+
+function changeImgR2() {
+    var img2 = document.getElementById('changer');
+    img2.src = 'nodel 1.png';
+    var documentRight2 = document.getElementById('info-text-3');
+    documentRight2.style.display = 'flex';
+    var mainImg = document.getElementById('info-text-0');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-1');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-2');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-4');
+    mainImg.style.display = 'none';
+}
+function changeImgL2() {
+    var img_left2 = document.getElementById('changer');
+    img_left2.src = 'nodel 3.png';
+    var documentLeft2 = document.getElementById('info-text-4');
+    documentLeft2.style.display = 'flex';
+    var mainImg = document.getElementById('info-text-0');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-1');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-2');
+    mainImg.style.display = 'none';
+    var mainImg = document.getElementById('info-text-3');
+    mainImg.style.display = 'none';
+}
+
+// swiper menu
